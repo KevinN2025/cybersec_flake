0558dc648596a2b2b5dc94c5689cd5626ac69c52
kevin noguera <kevin5631noguera@gmail.com>
2025-10-25 23:41:09 -0400

    Formatted the file a bit

--- flake.nix
+++ flake.nix
@@ -18,10 +18,9 @@
           buildInputs = with pkgs; [
               [zap metasploit wifite2 aircrack-ng sqlmap bloodhound responder john
               hashcat hashcat-utils badkeys pwntools mx-takeover cyberchef hexyl unifi
-              shorewall shadow waf wafw00f honeytrap crowdsec snort
-              suricata bettercap python313Packages.dpkt haskellPackages.nemesis yas
-              zeek rustscan python313Packages.angr detect-it-easy capstone scilla
-              commix]
+              shorewall shadow waf wafw00f honeytrap crowdsec snort suricata bettercap
+              python313Packages.dpkt haskellPackages.nemesis yas zeek rustscan
+              python313Packages.angr detect-it-easy capstone scilla commix]
           ];
         };
       }
